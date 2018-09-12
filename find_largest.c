530ada614cff72272a96e0d553484c8e073cc30b
Michael Wong <michaelzhwong@gmail.com>
2018-09-12 12:15:43 -0400

    Review on C basics

---
+++ find_largest.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void find_largest_in_array(int *A_array, int A_array_size, int *largest_pt) {
+  *largest_pt = *A_array; // A[0]
+  for (int i = 0; i < A_array_size; i++) {
+    if(A_array[i] > *largest_pt) {
+      *largest_pt = A_array[i];
+    }
+  }
+}
+
+int main() {
+  int i = 90;
+  int j = -4;
+  int k = 69;
+  int l = 32;
+
+  int A_array[4] = {i, j, k, l};
+
+  int largest;
+
+  int A_array_size = (int) (sizeof(A_array)/sizeof(A_array[0]));
+
+  find_largest_in_array(A_array, A_array_size, &largest);
+
+  printf("Largest is %d\n", largest);
+
+  return 0;
+
+}
